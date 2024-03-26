78f15f7b70480669950d2764babdd25f58b470b5
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-03-27 03:03:17 +0530

    updated with iterative approach

--- Binary_Strees/235_Lca.cpp
+++ Binary_Strees/235_Lca.cpp
@@ -9,8 +9,8 @@ using namespace std;
       TreeNode *right;
       TreeNode(int x) : val(x), left(NULL), right(NULL) {}
   };
- 
 
+// approach 1 : recursive way
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -23,5 +23,32 @@ public:
             return lowestCommonAncestor(root->left,p,q);
         }
         return root;
+    }
+};
+
+// approach 2 : iterative way
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root!=NULL){
+            if(root->val>p->val and root->val >q->val){
+                root=root->left;
+            }else if(root->val < p->val and root->val < q->val ){
+                root=root->right;
+            }
+            else{
+                return root;
+            }
+        }
     }
 };

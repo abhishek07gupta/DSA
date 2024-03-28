989885d04a3543fe25e7d44bec740a02a912192e
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-03-29 01:30:15 +0530

    leetcode_2958

---
+++ Sliding_Window/2958_Subarray_with_atmost_K.cpp
@@ -0,0 +1,32 @@
+#include<vector>
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+// approach 1 : sliding window and hashmap
+
+class Solution {
+public:
+    Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int,int> mp ; 
+        int n = nums.size();
+
+        int l = 0 ,r = 0,count=INT_MIN;
+        while(r<n){
+            int cur = nums[r++];
+            mp[cur]++;
+            while(l<r & mp[cur]>k){
+                mp[nums[l++]]--;
+            }
+            count = max(count,r-l);
+        }
+
+        return count;
+    }
+};
